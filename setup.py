6cbc2043b82a9991cf21f161acc9d869ec437a36
Vít Novotný <witiko@mail.muni.cz>
2022-05-26 17:49:35 +0200

    Use Transformers 4.16.2

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     zip_safe=True,
     install_requires=[
         "torch>=1.7",
-        "transformers==4.16.1",
+        "transformers==4.16.2",
         "sentencepiece",
     ],
     test_require=[
